6aa6285d2096646402fa1a70a3c2fdd6b3094dba
Lolz <92547513+AwesomePotato23@users.noreply.github.com>
2021-10-29 10:21:35 -0500

    More Python Practices.

--- lolz.py
+++ lolz.py
@@ -3,3 +3,10 @@ var = "awesome"
 print("This is " + var)
 var2 = "LolzTheSpooky"
 print("Follow " + var2 + " on WasteOf!")
+print("Ok so like once I almost was ran over by a car")
+feeling = scared
+print("I felt " + feeling + " lol")
+
+#Yes I actually was scared btw lmao
+
+print("Ok... uh")
