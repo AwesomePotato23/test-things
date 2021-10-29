9232d0c413c5bf1da0afdd7b2b370860a10368fc
Lolz <92547513+AwesomePotato23@users.noreply.github.com>
2021-10-29 10:26:16 -0500

    Debug this if I am stupid.

--- lolz.py
+++ lolz.py
@@ -10,3 +10,14 @@ print("I felt " + feeling + " lol")
 #Yes I actually was scared btw lmao
 
 print("Ok... uh")
+
+if 3 > 1:
+  print("3 is greater than 1")
+  
+def myFunc():
+  print("I am LolzTheSpooky :)")
+  
+intro = "yes"
+
+if (intro) = "yes"
+  myFunc()
