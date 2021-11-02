0b5bebca2af8b964fa54c1cadc58a2f1533994fb
Lolz <92547513+AwesomePotato23@users.noreply.github.com>
2021-11-02 09:26:13 -0500

    Fixed Bugs :)

--- lolz.js
+++ lolz.js
@@ -5,11 +5,15 @@ alert("Hello");
 alert("This is a JS file.");
 alert("So you can leave.");
 
-action = "leave"
+action = "leave";
 
 alert("So can you " + action);
 alert("Why aren't you leaving!?!");
 alert("LEAVE PLEASEEEE");
 
-var2 = "ok bye"
+var2 = "ok bye";
 alert(var2);
+
+alert("y u still here");
+var3 = "leave";
+alert(var3 + " now");
